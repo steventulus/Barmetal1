b2e6ae77acb0821dcc196de93ffe37613a135f84
Steven <steventulus14@gmail.com>
2025-09-04 00:06:28 +0700

    new timer

--- Src/main.c
+++ Src/main.c
@@ -36,10 +36,10 @@ int main(void) {
     while (1) {
         // LED ON (active low → reset bit)
         GPIOC->BSRR = (1 << 13) << 16;  
-        delay(50);
+        delay(90);
 
         // LED OFF (set bit)
         GPIOC->BSRR = (1 << 13);       
-        delay(500);
+        delay(100);
     }
 }
